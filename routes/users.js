46f6300be1fa1ea16419031d08b47ac671362a68
marijkevanhell <jukevanhell@gmail.com>
2023-05-26 15:30:07 -0700

    Merge branch 'database-migrations' of github.com:alinebellozo/buy-sell-listing-website into routes. & Update routes. Co-authored-by: Aline Bellozo <alinebellozo@users.noreply.github.com>

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,25 @@
 
 const express = require('express');
 const router  = express.Router();
+const app = express();
+const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
+//const { users, widgets } = require("../db/seeds");
+
+// middleware
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["secretkey"],
+    //expires after 24 hrs
+    maxAge: 24 * 60 * 60 * 1000
+  })
+);
+
+// routes
 
 router.get('/', (req, res) => {
   res.render('buyers');
@@ -28,7 +47,7 @@ app.get('/login/:id', (req, res) => {
 // get a certain buyer profile
 app.get('buyers/:id', (req, res) => {
   if (!req.session.buyer_id) {
-    return res.status(400).send("You are not logged in");
+    return res.status(400).send("You don't have an account, please sign up first");
   };
 
 
@@ -36,6 +55,9 @@ app.get('buyers/:id', (req, res) => {
 
 // edit a buyer profile
 app.post('buyers/:id/edit', (req, res) => {
+  if (!req.session.buyer_id) {
+    return res.status(400).send("You are not logged in");
+  }
 
 });
 
@@ -46,6 +68,20 @@ app.post('buyers', (req, res) => {
 
 // delete a buyer profile
 app.post('buyers/:id/delete', (req, res) => {
+  if (!req.session.buyer_id) {
+    return res.status(400).send("You are not logged in");
+  }
+
+  if (!users[req.params.id]) {
+    return res.status(400).send("Looks like you don't have an account yet");
+  }
+
+  // if (req.session.buyer_id !== users[req.params.id].buyer_id) {
+  //   return res.status(400).send("You don't have permission to delete this account");
+  // }
+
+  delete users[req.params.id];
+  res.redirect("/");
 
 });
 
