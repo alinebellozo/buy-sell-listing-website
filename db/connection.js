7e12c6b14e2119183cd4fd03a5e057302f6e2225
marijkevanhell <jukevanhell@gmail.com>
2023-05-29 16:25:29 -0700

    Update connection.js file: -add more functions

--- db/connection.js
+++ db/connection.js
@@ -13,4 +13,51 @@ const db = new Pool(dbParams);
 
 db.connect();
 
+const getUserWithEmail = function(email) {
+  const queryString = `SELECT * FROM users
+    WHERE email = $1`;
+  const values = [email];
+  return pool
+    .query(queryString, values)
+    .then((res) => res.rows[0] || null)
+    .catch((err) => err);
+};
+
+const getUserWithId = function(id) {
+  const queryString = `SELECT * FROM users
+    WHERE id = $1`;
+  const values = [id];
+  return pool
+    .query(queryString, values)
+    .then((res) => res.rows[0] || null)
+    .catch((err) => err);
+};
+
+const addUser = function(user) {
+  const queryString = `INSERT INTO users (name, email, password)
+    VALUES ($1, $2, $3)
+    RETURNING *`;
+  const values = [user.name, user.email, user.password];
+  return pool
+    .query(queryString, values)
+    .then((res) => res.rows[0])
+    .catch((err) => err);
+};
+
+const getAllFavourites = function() {
+
+  return pool
+    .query(queryString, values)
+    .then((res) => res.rows)
+    .catch((err) => err);
+};
+
+const seeItemsMarkedAsSold = function() {
+
+  return pool.query().then((res) => {
+    console.log(res.rows);
+    return res.rows;
+  });
+};
+
 module.exports = db;
