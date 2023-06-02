0dfa7de30633ea4cb8cc40d7351026fdb3fb4a96
Jeremy Lee <jeremylee0615@gmail.com>
2023-06-02 16:12:09 -0700

    fixed mug ID numbers

--- db/seeds/03_mugs.sql
+++ db/seeds/03_mugs.sql
@@ -1,4 +1,4 @@
-INSERT INTO mugs (seller_id, price, name, quantity, in_stock, image) VALUES ('01', '5', 'Pastel Mug', '4', TRUE, '../images/blue.jpg' );
-INSERT INTO mugs (seller_id, price, name, quantity, in_stock, image) VALUES ('02', '9', 'Sunny Days Mug', '1', TRUE, '../images/heart.jpg');
+INSERT INTO mugs (seller_id, price, name, quantity, in_stock, image) VALUES ('01', '$5.00', 'Pastel Mug', '4', TRUE, '../images/blue.jpg' );
+INSERT INTO mugs (seller_id, price, name, quantity, in_stock, image) VALUES ('02', '$9.00', 'Sunny Days Mug', '1', TRUE, '../images/heart.jpg');
 
 
